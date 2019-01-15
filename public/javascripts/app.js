2636f77c7064d39aa57fc3e8784adfc2f17e802f
AnhTuanNguyenn <fictionbh@gmail.com>
2019-01-15 10:44:00 +0100

    updatev1

--- public/javascripts/app.js
+++ public/javascripts/app.js
@@ -10,9 +10,9 @@ $(function () {
         var gender = $('input[name="gender"]:checked').val();
         var tel = $("#tel").val();
         var terms = $('input[name="terms"]:checked').val();
-        var ques1 = $("ques1").val();
-        var ques2 = $("ques2").val();
-        var ques3 = $("ques3").val();
+        var ques1 = $("#ques1").val();
+        var ques2 = $("#ques2").val();
+        var ques3 = $("#ques3").val();
         
         if (!fullname || !email || !address || !dob || !job || !gender || !tel || !ques1 || !ques2 || !ques3) {
             $("#msgDiv").show().html("Tous les champs sont requis.");
